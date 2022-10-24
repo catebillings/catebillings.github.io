861412658f8a772254c83fdc3f0cf82c0abe9b14
catebillings <cabi1185@colorado.edu>
2022-10-23 19:31:10 -0600

    assignment9

--- assignments/assignment9/main.js
+++ assignments/assignment9/main.js
@@ -9,11 +9,11 @@ const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const alts = {
-  'pic1.jpg' : 'Closeup of a human eye',
-  'pic2.jpg' : 'Rock that looks like a wave',
-  'pic3.jpg' : 'Purple and white pansies',
-  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
-  'pic5.jpg' : 'Large moth on a leaf'
+  'pic1.jpg' : 'Sidewalk with trees',
+  'pic2.jpg' : 'Mushrooms in a patch of grass',
+  'pic3.jpg' : 'Purple bush in a garden',
+  'pic4.jpg' : 'A bunch of dark green leaves',
+  'pic5.jpg' : 'A weed growing in the sidewalk crack'
 }
 
 /* Looping through images */
